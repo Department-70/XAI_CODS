7cc11d7f97b41b1a4a9ea4088962b74bba1a7b3a
Debra <enjelika@users.noreply.github.com>
2023-05-23 09:42:07 -0500

    Update XAI_Decision_Hierarchy.py

--- FACE-P1/XAI_Decision_Hierarchy.py
+++ FACE-P1/XAI_Decision_Hierarchy.py
@@ -20,20 +20,24 @@ import matplotlib.pyplot as plt
 from skimage.measure import label, regionprops, find_contours
 
 """ folder locations for binary maps, ranking maps, and object parts json """
-image_root = './dataset/COD10K_FixTR/Image/'
-gt_root = './dataset/COD10K_FixTR/GT/'
-fix_root = './dataset/COD10K_FixTR/Fix/'
-obj_parts = './dataset/CORVIS-parts-dataset/'
+# image_root = './dataset/COD10K_FixTR/Image/'
+# gt_root = './dataset/COD10K_FixTR/GT/'
+# fix_root = './dataset/COD10K_FixTR/Fix/'
+# obj_parts = './dataset/CORVIS-parts-dataset/'
+
+image_root = './dataset/train/Imgs/'
+gt_root = './dataset/train/GT/'
+fix_root = './dataset/train/Fix/'
 
 # Example image
 NonCAM = 'COD10K-NonCAM-5-Background-5-Vegetation-4854.png'
 CAM = 'COD10K-CAM-3-Flying-55-Butterfly-3330.png'
 
 # Custom colormap for fixation maps
-RdBl = color.LinearSegmentedColormap.from_list('blGrRdBl', ["black", "black", "black", "red"])
+RdBl = color.LinearSegmentedColormap.from_list('blGrRdBl', ["black", "black", "red", "red"])
 # plt.colormaps.register(RdBl)
 
-blGrRdBl = color.LinearSegmentedColormap.from_list('blGrRdBl', ["black", "blue", "green", "red"])
+blGrRdBl = color.LinearSegmentedColormap.from_list('blGrRdBl', ["black", "blue", "green", "red", "red"])
 # plt.colormaps.register(blGrRdBl)
 
 if not os.path.exists("figures"):
@@ -41,6 +45,73 @@ if not os.path.exists("figures"):
     
 if not os.path.exists("bbox_figures"):
     os.mkdir("bbox_figures")
+    
+if not os.path.exists("jsons"):
+    os.mkdir("jsons")
+    
+if not os.path.exists("outputs"):
+    os.mkdir("outputs")
+
+
+"""
+===================================================================================================
+    Helper function
+        - Adds the XAI message as a label onto a copy of th esegmented image
+===================================================================================================
+"""
+from PIL import ImageDraw
+from PIL import ImageFont
+
+def add_label(image, label_text, label_position):
+
+    # Create a drawing object
+    draw = ImageDraw.Draw(image)
+
+    # Define the font and font size
+    font = ImageFont.truetype('arial.ttf', 20)
+
+    # Draw the label on the image
+    draw.text(label_position, label_text, fill=(255, 255, 255), font=font)
+
+    # Save the image with the label
+    image.save('labeled_image.jpg')
+
+
+"""
+===================================================================================================
+    Helper function
+        - Adds the XAI message as a label onto a copy of the segmented image
+===================================================================================================
+"""
+
+def segment_image(original_image, mask_image, color=(255, 0, 0)):
+    
+    # Conver the mask to mode 1
+    mask_image = mask_image.convert('1')
+    
+    # Check that the mask and image have the same size
+    if original_image.size != mask_image.size:
+        raise ValueError('Image and mask must have the same size.')
+        
+    # Check that the mask has the correct mode
+    if mask_image.mode != '1':
+        raise ValueError('Mask must be a binary image.')
+        
+    # Convert the mask to a numpy array
+    mask_array = np.array(mask_image, dtype=bool)
+    
+    # Create a copy of the original image
+    highlighted_image = np.array(original_image).copy()
+    
+    # Apply the segmentation to the image
+    indices = np.nonzero(mask_array)
+    highlighted_image[indices] = color
+    
+    # Convert the numpy array back to a PIL Image
+    highlighted_image = Image.fromarray(highlighted_image)
+
+    return highlighted_image
+
 
 """
 ===================================================================================================
@@ -161,9 +232,6 @@ def levelThree(fixation_map, message):
 ===================================================================================================
 """
 def levelTwo(filename, original_image, all_fix_map, fixation_map, message):
-    # print("In level 2")
-    # print("Detecting weak camouflaged areas...")
-    # print("")
     
     # Mask the red area(s) in the fixation map (weak camouflaged area(s))
     fig, axis = plt.subplots(1,2, figsize=(12,6))
@@ -219,9 +287,9 @@ def levelTwo(filename, original_image, all_fix_map, fixation_map, message):
         index = index + 1
     
     # the json file to save the output data   
-    save_file = open(filename+".json", "w")  
-    json.dump(data, save_file, indent = 6)  
-    save_file.close() 
+    with open("jsons/"+filename+".json", "w")  as f: 
+        json.dump(data, f, indent = 6)  
+        f.close() 
         
     # Figure of original image and marked image
     fig, axis = plt.subplots(1,2, figsize=(12,6))
@@ -249,17 +317,15 @@ def levelTwo(filename, original_image, all_fix_map, fixation_map, message):
 ===================================================================================================
 """
 def levelOne(filename, binary_map, all_fix_map, fix_image, original_image, message):
-    # print("")
-    # print("In level 1")
 
     # Does the numpy array contain any non-zero values?
-    all_zeros = not img_np.any()
+    all_zeros = not binary_map.any()
     
     if all_zeros:
         # No object detected, no need to continue to lower levels
         message += "No object present. \n"
         # print("No object present.")
-        output = "1"
+        output = message
     else:
         # Object detected, continue to Lvl 2
         message += "Object present. \n"
@@ -268,6 +334,51 @@ def levelOne(filename, binary_map, all_fix_map, fix_image, original_image, messa
         output = levelTwo(filename, original_image, all_fix_map, fix_image, message)
         
     return output
+
+
+"""
+===================================================================================================
+    XAI Function
+===================================================================================================
+"""
+def xaiDecision(file, counter):
+    
+    # Filename
+    file_name = os.path.splitext(file.name)[0]
+
+    # XAI Message
+    message = "Decision for " + file_name + ": \n"
+    
+    # Gather the images: Original, Binary Mapping, Fixation Mapping
+    original_image = cv2.imread(image_root + file_name + '.jpg')
+    dim = original_image.shape
+    
+    if os.path.exists(gt_root + file_name + '.png'):
+        bm_image = Image.open(gt_root + file_name + '.png')
+    else:
+        bm_image = np.zeros((dim[1], dim[0],3), np.uint8)
+    
+    if os.path.exists(fix_root + file_name + '.png'):
+        fix_image = Image.open(fix_root + file_name + '.png')
+    else:
+        fix_image = np.zeros((dim[1], dim[0],3), np.uint8)
+    
+    # Normalize the Binary Mapping
+    trans_img = np.transpose(bm_image)
+    img_np = np.asarray(trans_img)/255
+    
+    # Preprocess the Fixation Mapping
+    weak_fix_map = findAreasOfWeakCamouflage(fix_image)
+    all_fix_map = processFixationMap(fix_image)
+    
+    output = levelOne(file_name, img_np, all_fix_map, weak_fix_map, original_image, message)
+
+    org_image = Image.open(image_root + file_name + '.jpg')
+    segmented_image = segment_image(org_image, fix_image, color=(255, 0, 0))
+    add_label(segmented_image, output, (15, 15))
+    segmented_image.save('outputs/segmented_'+ file_name +'.jpg')
+    
+    return message
 
 
 """
@@ -285,7 +396,7 @@ if __name__ == "__main__":
         file_name = os.path.splitext(files.name)[0]
     
         # XAI Message
-        message = "Decision " + str(counter) + ": \n"
+        message = "Decision for " + file_name + ": \n"
         
         # Gather the images: Original, Binary Mapping, Fixation Mapping
         original_image = cv2.imread(image_root + file_name + '.jpg')
@@ -311,10 +422,13 @@ if __name__ == "__main__":
         
         output = levelOne(file_name, img_np, all_fix_map, weak_fix_map, original_image, message)
     
-        print(output)
+        org_image = Image.open(image_root + file_name + '.jpg')
+        segmented_image = segment_image(org_image, fix_image, color=(255, 0, 0))
+        add_label(segmented_image, output, (15, 15))
+        segmented_image.save('outputs/segmented_'+ file_name +'.jpg')
         
         counter += 1
         
-        if counter == 21:
+        if counter == 3041:
             break
     
